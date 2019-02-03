5f48a18dfaaef7f9039084457b3da66b108b8933
fallen potato <cruzjon32@gmail.com>
2019-02-03 14:19:48 -0700

    hello

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
+	app.get("/", function(req, res) {});
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
@@ -17,7 +18,9 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+			dbExample
+		) {
       res.json(dbExample);
     });
   });
