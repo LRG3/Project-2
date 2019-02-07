075492f6a809b2c973062cc1bb3f5667a351c9c1
fallen potato <cruzjon32@gmail.com>
2019-02-06 20:35:14 -0700

    the

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql")
+var connection
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "fallenpotato",
+        database: "raven_db"
+    })
+}
+
+
+
+connection.connect(function(error) {
+    if (error) throw (error)
+    console.log("Using localhost as " + connection.threadId)
+})
+
+module.exports = connection
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,52 +1,65 @@
-var db = require("../models");
 var axios = require("axios")
+var orm = require("../models/index")
 
 module.exports = function (app) {
-  // Load index page
   app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+    res.render("index")
+  })
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  var redirectUrl = "http://localhost:8083/callback"
-    var clientId = "873845158fb34ac39c8fedba710f1354"
   app.get("/login", function (req, res) {
-    var scopes = "user-read-private user-read-email"
-    
-    
+    var scopes = "user-read-private user-read-email playlist-read-private"
+    var redirectUrl = "http://localhost:8083/callback"
+    var clientId = "873845158fb34ac39c8fedba710f1354"
+
     res.redirect("https://accounts.spotify.com/authorize" +
       "?response_type=code" + "&client_id=" +
       clientId + (scopes ? "&scopes=" + encodeURIComponent(scopes) : "") +
       "&redirect_uri=" + encodeURIComponent(redirectUrl))
 
-  });
+  })
 
-  app.post("https://accounts.spotify.com/api/token")
 
   app.get("/callback", function (req, res) {
+    var redirectUrl = "http://localhost:8083/callback"
+    console.log(Buffer.from("873845158fb34ac39c8fedba710f1354:d0d6e4bcd8014736b1de3b4d1cc2d35b").toString("base64"))
+
+    var requestData = {
+      grant_type: "authorization_code",
+      code: req.query.code,
+      redirect_uri: redirectUrl
+    }
+
+    var paramData = Object.keys(requestData).map(function (key) {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(requestData[key])
+    }).join('&')
+
+    axios.request({
+      url: "https://accounts.spotify.com/api/token",
+      data: paramData,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        "Authorization": "Basic " + Buffer.from("873845158fb34ac39c8fedba710f1354:d0d6e4bcd8014736b1de3b4d1cc2d35b").toString("base64")
+      },
+      method: "post"
+    }).then(function (response) {
+      axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + response.data.access_token
+        }
+      }).then(function (response) {
+        console.log(response)
+        var userName = response.data.display_name
+        res.redirect("/redirected")
+      })
+    })
+
+  })
+
+
+  app.get("/redirected", function(req, res) {
     res.render("login")
+  })
 
-  });
+}
 
 
-
-  // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   res.render("404");
-  // });
-};
